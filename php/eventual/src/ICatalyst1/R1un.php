eb71cf69430390ceb5078d1f1822670961fe3724
Ken McHugh <kmchugh@icatalyst.co.uk>
2013-08-01 22:43:59 +0800

    Updates for composer and phpunit

---
+++ php/eventual/src/ICatalyst1/R1un.php
@@ -0,0 +1,9 @@
+<?php
+namespace ICatalyst;
+
+class Run
+{
+
+}
+
+?>
--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Setup of environment for development
 
+# Install macports for mac?
+# install php
+# install openssl for php
+
 # install composer
 curl -sS https://getcomposer.org/installer | php -d detect_unicode=Off -- --install-dir=php/eventual
+
+# install the required modules
+cd ./eventual/
+php composer.phar install
+cd ..
