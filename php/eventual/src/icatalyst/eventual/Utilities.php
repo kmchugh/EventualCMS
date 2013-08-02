85d51733d50e238ef4515d285d457bfbeef2b983
Ken McHugh <kmchugh@ICatalyst.co.uk>
2013-08-02 12:37:49 +0800

    Refactor of loadClass, completion of CLI core

---
+++ php/eventual/src/icatalyst/eventual/Utilities.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Utility class for oft used static functions
+ */
+
+namespace icatalyst\eventual;
+
+class Utilities
+{
+    /**
+     * Loads the specified class files using include_once.
+     * The order of loading will be
+     * - EVENTUAL_ROOT + namespace if a namespace is given
+     * - namespace if one is given
+     * - paths from the php include_path in order
+     *
+     * This load function is PSR-0 compliant
+     *
+     * @param $tcClass the class name to load, if fully qualified this will respect the namespace
+     * @return null|string returns the name of the class file used, or NULL if no class was loaded
+     */
+    static public function loadClass($tcClass)
+    {
+        // Extract the namespace and class name
+        $laClass = explode('\\', $tcClass);
+        $lcClassName = str_replace('\\', DIRECTORY_SEPARATOR, $laClass[count($laClass)-1]);
+        $lcNameSpace = count($laClass) == 1 ? NULL : implode(DIRECTORY_SEPARATOR, array_slice($laClass, 0, count($laClass)-1));
+
+        // Include the include_path for autoloading
+        $lcIncludePath = (!is_null($lcNameSpace) ? EVENTUAL_ROOT.$lcNameSpace.PATH_SEPARATOR.$lcNameSpace.PATH_SEPARATOR : '').ini_get('include_path');
+        foreach (explode(PATH_SEPARATOR, $lcIncludePath) as $lcPath)
+        {
+            $lcClassFile = $lcPath.DIRECTORY_SEPARATOR.$lcClassName.'.php';
+            if (is_readable($lcClassFile))
+            {
+                include_once($lcClassFile);
+                return $lcClassFile;
+            }
+        }
+        return NULL;
+    }
+}
