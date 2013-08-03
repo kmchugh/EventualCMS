639e8042a41aebcb9c2c3833762cac0881f92f8d
Ken McHugh <kmchugh@icatalyst.co.uk>
2013-08-03 10:39:11 +0800

    Cleanup

--- php/eventual/tests/unit/icatalyst/Bootstrap.php
+++ php/eventual/tests/unit/icatalyst/Bootstrap.php
@@ -3,19 +3,6 @@
  * Bootstrap is used for launching tests
  **/
 
-// Define the app constants
-defined('EVENTUAL_ROOT') || define('EVENTUAL_ROOT', str_replace('\\', DIRECTORY_SEPARATOR, realpath(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR));
+require_once(str_replace('\\', DIRECTORY_SEPARATOR, realpath(dirname(dirname(dirname(dirname(__FILE__))))).'\\src\\icatalyst\\exec'));
 
-// Allow access to STDIN from fcgi
-defined('STDIN') || define('STDIN', fopen('php://stdin', 'r'));
-
-// Allow access to STDOUT from fcgi
-// defined('STDOUT') || define('STDOUT', fopen('php://stdout', 'w'));
-
-// Include the base utilities class and register the autoloader
-require_once(EVENTUAL_ROOT.'icatalyst'.DIRECTORY_SEPARATOR.'eventual'.DIRECTORY_SEPARATOR.'Utilities.php');
-spl_autoload_register('\icatalyst\eventual\Utilities::loadClass');
-
-// Create the console bootstrap
-$loBootstrap = new \icatalyst\eventual\console\Bootstrap();
 ?>
